8a9bed3f7065213d8fd36b83ace4ce9398c88270
Jay Houn <jayhoun@gmail.com>
2018-01-23 11:54:42 -1000

    added 7 array problems

---
+++ app.js
@@ -0,0 +1,53 @@
+// Create sanity check below
+
+
+// # 1
+// find the length of the array
+var winterOlympicSports = ['snowboarding', 'hockey', 'ice skating', 'curling'];
+    // write code below
+
+console.log() //4
+
+// # 2
+// Get index 2 by accessing the sports array
+var sports = ['basketball', 'volleyball', 'soccer', 'tennis'];
+    // write code below
+
+console.log() //'soccer'
+
+// # 3
+// Change 'helmet' to 'sweat band'
+var tennisEquipment= ['raquet', 'tennis balls', 'net', 'helmet'];
+    // write code below
+
+console.log() // ['raquet', 'tennis balls', 'net', 'sweat band'];
+    
+// # 4
+// Use push method to add 'Japan' 
+var countries = ['Mexico', 'Spain', 'Thailand', 'USA', 'Brazil'];
+    // write code below
+
+console.log() // ['Mexico', 'Spain', 'Thailand', 'USA', 'Brazil', 'Japan'];
+
+// # 5
+// Use pop method. Console.log array and value that was popped off
+var states = ['California', 'Mississippi', 'Hawaii', 'Michigan', 'New York'];
+    // write code below
+
+console.log() // [California', 'Mississippi', 'Hawaii', 'Michigan', 'New York'];
+console.log() // 'New York'
+
+// # 6
+// Use unshift method to add 'Swahili'
+var languages = ['Dutch', 'Mongolian', 'English', 'Finnish', 'Spanish'];
+    // write code below
+
+console.log() // ['Swahili', 'Dutch', 'Mongolian', 'English', 'Finnish', 'Spanish']
+
+// # 7
+// Use shift method. Console.log array and value that was shifted
+var cities = ['Honolulu', 'Los Angeles', 'Philidelphia', 'New Orleans', 'Denver'];
+    // write code below
+
+console.log() // ['Los Angeles', 'Philidelphia', 'New Orleans', 'Denver'];
+console.log() // 'Honolulu'
