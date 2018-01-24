beecc505f6d37cec98cf87b58a289b19e8d04bea
Jay Houn <jayhoun@gmail.com>
2018-01-24 09:13:17 -1000

    added up to 16 questions

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var winterOlympicSports = ['snowboarding', 'hockey', 'ice skating', 'curling'];
 
 console.log() //4
 
+
 // # 2
 // Get index 2 by accessing the sports array
 var sports = ['basketball', 'volleyball', 'soccer', 'tennis'];
@@ -15,19 +16,22 @@ var sports = ['basketball', 'volleyball', 'soccer', 'tennis'];
 
 console.log() //'soccer'
 
+
 // # 3
 // Change 'helmet' to 'sweat band'
 var tennisEquipment= ['raquet', 'tennis balls', 'net', 'helmet'];
     // write code below
 
 console.log() // ['raquet', 'tennis balls', 'net', 'sweat band'];
-    
+  
+
 // # 4
 // Use push method to add 'Japan' 
 var countries = ['Mexico', 'Spain', 'Thailand', 'USA', 'Brazil'];
     // write code below
 
 console.log() // ['Mexico', 'Spain', 'Thailand', 'USA', 'Brazil', 'Japan'];
+
 
 // # 5
 // Use pop method. Console.log array and value that was popped off
@@ -37,12 +41,14 @@ var states = ['California', 'Mississippi', 'Hawaii', 'Michigan', 'New York'];
 console.log() // [California', 'Mississippi', 'Hawaii', 'Michigan', 'New York'];
 console.log() // 'New York'
 
+
 // # 6
 // Use unshift method to add 'Swahili'
 var languages = ['Dutch', 'Mongolian', 'English', 'Finnish', 'Spanish'];
     // write code below
 
 console.log() // ['Swahili', 'Dutch', 'Mongolian', 'English', 'Finnish', 'Spanish']
+
 
 // # 7
 // Use shift method. Console.log array and value that was shifted
@@ -51,3 +57,77 @@ var cities = ['Honolulu', 'Los Angeles', 'Philidelphia', 'New Orleans', 'Denver'
 
 console.log() // ['Los Angeles', 'Philidelphia', 'New Orleans', 'Denver'];
 console.log() // 'Honolulu'
+
+
+// # 8
+// Sort array from lowest to highest
+var numbers = [5, 3, 1, 2, 8, 4];
+    // write code below
+
+console.log() // [1, 2, 3, 4, 5, 8];
+
+
+// # 9
+// Reverse the array
+var counting = ['one', 'two', 'three', 'four', 'five'];
+    // write code below
+
+console.log() // ['five', 'four' , 'three', 'two', 'one'];
+
+
+// # 10
+// Return index number of 'vanilla'
+var iceCreamFlavors = ['rainbow sherbert', 'rocky road', 'cookies and cream', 'vanilla', 'strawberry'];
+    // write code below
+
+console.log() // 4;
+
+
+// # 11
+// Return index number of 'green tea'
+var iceCreamFlavors = ['rainbow sherbert', 'rocky road', 'cookies and cream', 'vanilla', 'strawberry'];
+    // write code below
+
+console.log() // -1;
+
+
+// # 12
+// Use slice method to remove 'yellow', 'black', 'red'
+var colors = ['green', 'yellow', 'black', 'red', 'pink'];
+    // write code below
+
+console.log() // ['green', 'pink'];
+
+
+// # 13
+// Use splice method to delete the value 'lion'. Console.log array and the item that was removed
+var animals = ['bird', 'dog', 'cat', 'lion', 'wallaby', 'sloth'];
+    // write code below
+
+console.log() // ['bird', 'dog', 'cat', 'wallaby', 'sloth'];
+console.log() // ['lion']
+
+
+// # 14
+// Use splice method to delete 2 values 'wallaby' & 'sloth'. Console.log array and the item that was removed
+var animals = ['bird', 'dog', 'cat', 'lion', 'wallaby', 'sloth'];
+    // write code below
+
+console.log() // ['bird', 'dog', 'cat','lion'];
+console.log() // ['wallaby', 'sloth']
+
+
+// # 15
+// Use splice method to add the value 'Stegosaurus' before 'Velociraptor'
+var dinosaurs = ['Tyrannosaurus', 'Velociraptor', 'Triceratops', 'Pterodactyl', 'Brachiosaurus'];
+    // write code below
+
+console.log() // ['Tyrannosaurus', 'Stegosaurus', 'Velociraptor', 'Triceratops', 'Pterodactyl', 'Brachiosaurus']
+
+
+// # 16
+// Use splice method to delete the value 'Pterodactyl', then add 'Stegosaurus' and 'Diplodocus'
+var dinosaurs = ['Tyrannosaurus', 'Velociraptor', 'Triceratops', 'Pterodactyl', 'Brachiosaurus'];
+    // write code below
+
+console.log() // ['Tyrannosaurus', 'Stegosaurus', 'Velociraptor', 'Triceratops', 'Stegosaurus', 'Diplodocus','Brachiosaurus']
